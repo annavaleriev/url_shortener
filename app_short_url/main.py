01f2741b1a587b83dc2e8bdb864bdcd60c7f9c96
annavaleriev <filenko.a@gmail.com>
2025-07-15 01:15:49 +0300

    [feature/models_and_create_short_url] Сделаны корректировки в get_url_from_db и redirect_to_original_urlю

--- app_short_url/main.py
+++ app_short_url/main.py
@@ -1,33 +1,37 @@
-from fastapi import FastAPI
-from starlette.status import HTTP_201_CREATED
+from fastapi import FastAPI, HTTPException
+from starlette import status
+from starlette.responses import RedirectResponse
+
 
 from app_short_url.models import URLResponse, URLRequest
-from app_short_url.storage import save_url_to_db
+from app_short_url.storage import save_url_to_db, get_url_from_db
 from app_short_url.utils import generate_shot_id
 
 app = FastAPI()
 
 
-#
-# @app.get("/")
-# def read_root():
-#     return {"message": "все работает!"}
-
-# url_db: Dict[str, str] = {}
-
 @app.post(
-    "/shorten",
+    "/shorten_url",
     summary="Получение короткого url",
     tags=["Основные ручки"],
     response_model=URLResponse,
-    status_code=HTTP_201_CREATED
+    status_code=status.HTTP_201_CREATED
 )
 async def create_short_url(data: URLRequest) -> URLResponse:
     """Создание короткой ссылки"""
-    # Здесь должна быть логика создания короткой ссылки
-    # Например, сохранение в базу данных и возврат сгенерированного id
-    short_url_id = generate_shot_id()  # Предполагается, что функция generate_shot_id импортирована из utils
-    save_url_to_db(short_url_id, str(data.origin_url))  # Функция для сохранения в базу данных
+    short_url_id = generate_shot_id()
+    save_url_to_db(short_url_id, str(data.origin_url))
     return URLResponse(short_url_id=short_url_id)
 
-# @app.get("/shorten/{short_url_id}", summary="Получение оригинального url", tags="Основные ручки")
+
+@app.get(
+    "/redirect/{short_url_id}",
+    summary="Редирект на оригинальный url с короткого url",
+    tags=["Основные ручки"]
+)
+async def redirect_to_original_url(short_url_id: str) -> RedirectResponse:
+    """Редирект на оригинальный url с короткого url"""
+    original_url = get_url_from_db(short_url_id)
+    if not original_url:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,  detail="Короткая ссылка не найдена")
+    return RedirectResponse(url=original_url, status_code=status.HTTP_307_TEMPORARY_REDIRECT)
